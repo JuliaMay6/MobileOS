830e7944a8a81b2a79251ec7ab72a0701bd007d8
JuliaMay6 <70854912+JuliaMay6@users.noreply.github.com>
2024-05-24 17:10:46 +0300

    Lesson8

---
+++ Lesson8/yandexdriver/src/main/java/ru/mirea/kosovaya/yandexdriver/App.java
@@ -0,0 +1,15 @@
+package ru.mirea.kosovaya.yandexdriver;
+
+import com.yandex.mapkit.MapKitFactory;
+
+import android.app.Application;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "3836296e-3ca8-40f0-9847-b31b971c0a33";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Set the api key before calling initialize on MapKitFactory.
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
